8c7e9ef95c472e80807f4181c672c3bbd2ef33f5
Jeremy Dryden <jeremy.dryden@gmail.com>
2019-02-15 12:29:58 -0500

    javascript - cycling through the different snippets

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,117 @@ const myapp = {};
 
 // Title, Code
 myapp.snippets = [];
+myapp.current = 0;
+// default data for book, identation is strange so that it
+// will display correctly
+myapp.defaultData = [
+   {
+      title: 'Smooth Scroll',
+      code:
+         `html {
+   scroll-behavior: smooth;
+}`
+   },
+   {
+      title: 'Gradient: Yellow Radial',
+      code:
+         `.gradient {
+  background-image:
+    radial-gradient(
+      yellow,
+      #f06d06
+    );
+}`
+   },
+   {
+      title: 'Hexagon with Shadow',
+      code:
+         `
+<div class="hexagon"><span></span></div>
 
+.hexagon {
+  width: 100px;
+  height: 55px;
+  position: relative;
+}
+
+.hexagon, 
+.hexagon:before, 
+.hexagon:after {
+  background: red;
+  box-shadow: 0 0 10px rgba(0,0,0,0.8);   
+}
+
+.hexagon:before,
+.hexagon:after {
+  content: "";
+  position: absolute;
+  left: 22px;
+  width: 57px;
+  height: 57px;
+  transform: rotate(145deg) skew(22.5deg);
+}
+
+.hexagon:before {
+  top: -29px;
+}
+
+.hexagon:after {
+  top: 27px;
+}
+
+.hexagon span {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100px;
+  height: 55px;
+  background: red;
+  z-index: 1;
+}`
+   }, {
+      title: 'Disorganized Paper Stack - CSS Tricks',
+      code:
+         `<div class="paper"></div>
+
+.paper {
+  background: #fff;
+  padding: 30px;
+  position: relative;
+}
+
+.paper,
+.paper::before,
+.paper::after {
+  /* Styles to distinguish sheets from one another */
+  box-shadow: 1px 1px 1px rgba(0,0,0,0.25);
+  border: 1px solid #bbb;
+}
+
+.paper::before,
+.paper::after {
+  content: "";
+  position: absolute;
+  height: 95%;
+  width: 99%;
+  background-color: #eee;
+}
+
+.paper::before {
+  right: 15px;
+  top: 0;
+  transform: rotate(-1deg);
+  z-index: -1;
+}
+
+.paper::after {
+  top: 5px;
+  right: -5px;
+  transform: rotate(1deg);
+  z-index: -2;
+}`
+   }
+];
 
 myapp.saveSnippet = function(snippet) {
    this.snippets.push(snippet);
@@ -14,6 +124,53 @@ myapp.removeSnippet = function(id) {
    // console.log(myapp.snippets);
 }
 
+myapp.loadSnippet = function(id) {
+   // if an id is passed, return that snippet
+   if(id) {
+      return this.snippets[id];
+   } else {
+      // else, return the current snippet
+      return this.snippets[myapp.current];
+   }
+}
+
+// load the snippet collection from memory or the dummy data
+myapp.loadSnippetArray = function() {
+   myapp.loadDefaultSnippets();
+}
+
+// A default array of data for testing and demo
+myapp.loadDefaultSnippets = function() {
+   myapp.snippets = myapp.defaultData;
+}
+
+// moving the current snippet pointer around
+myapp.moveToSnippet = function(command) {
+   switch (String(command).toLowerCase()) {
+      case "first":
+         myapp.current = 0;
+         myapp.loadSnippet();
+         break;
+      case "previous":
+         if(myapp.current > 0)
+            myapp.current = myapp.current - 1;
+         myapp.loadSnippet();
+         break;
+      case "next":
+         if(myapp.current < myapp.snippets.length - 1)
+            myapp.current += 1;
+         myapp.loadSnippet();
+         break;
+      case "last":
+         myapp.current = myapp.snippets.length - 1;
+         myapp.loadSnippet();
+         break;
+      default:
+         // nothing
+   }
+   console.log("current:" + myapp.current);
+}
+
 $(function() {
    let $snippetTitle    = $('#snippet-title');
    let $snippetCode     = $('#snippet-code');
@@ -23,10 +180,24 @@ $(function() {
    // let $chevronNext     = $('button-next');
    // let $chevronLast     = $('button-last');
 
+   // load defaults
+   myapp.loadSnippetArray();
+   
+   const loadCard = function() {
+      if(myapp.loadSnippet(myapp.current)) {
+         $snippetTitle.val(myapp.loadSnippet(myapp.current).title);
+         $snippetCode.val(myapp.loadSnippet(myapp.current).code);
+      }
+   }
+
+   // load the card at the start of the program
+   loadCard();
+
    $('form').on('submit', function(e) {
       // Prevent the form from submitting
       e.preventDefault();
    });
+
    
    // CRUD Buttons Start
    $('#new-snippet').on('click', function() {
@@ -34,6 +205,7 @@ $(function() {
       $snippetCode.val('');
    });
    
+   // todo: not quite working yet
    $('#delete-snippet').on('click', function() {
       myapp.removeSnippet(1);
    });
@@ -48,21 +220,26 @@ $(function() {
    // CRUD Buttons End
    
 
+
    // Navigation Buttons Start
    $('#button-first').on('click', function() {
-      console.log("first");
+      myapp.moveToSnippet("first");
+      loadCard();
    });
    
    $('#button-previous').on('click', function() {
-      console.log("previous");
+      myapp.moveToSnippet("previous");
+      loadCard();
    });
    
    $('#button-next').on('click', function() {
-      console.log("next");
+      myapp.moveToSnippet("next");
+      loadCard();
    });
    
    $('#button-last').on('click', function() {
-      console.log("last");
+      myapp.moveToSnippet("last");
+      loadCard();
    });
    // Navigation Button End
 });
